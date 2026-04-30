61ef063e035e32e23c55ab312ce1af224a636a75
matthewurbanik1 <matthewurbanik1@gmail.com>
2026-04-30 14:51:10 -0400

    Add files via upload

---
+++ Project4.java
@@ -0,0 +1,86 @@
+class Records {
+    private String company;
+    private int year;
+    private int employees_start;
+    private int employees_end;
+    private int new_hires;
+    private int layoffs;
+    private int net_change;
+    private double hiring_rate_pct;
+    private double attrition_rate_pct;
+    private double revenue_billion_usd;
+    private double stock_price_change_pct;
+    private double gdp_growth_us_pct;
+    private double unemployment_rate_us_pct;
+    private boolean is_estimated;
+    private String confidence_level;
+    private int data_quality_score;
+
+    public String getCompany(){
+        return company;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public int getEmployeesStart(){
+        return employees_start;
+    }
+
+    public int getEmployeesEnd(){
+        return employees_end;
+    }
+
+    public int getNewHires(){
+        return new_hires;
+    }
+
+    public int getLayoffs(){
+        return layoffs;
+    }
+
+    public int getNetChange(){
+        return net_change;
+    }
+
+    public double getHiringRatePct(){
+        return hiring_rate_pct;
+    }
+
+    public double getAttritionRatePct(){
+        return attrition_rate_pct;
+    }
+
+    public double getRevenueBillionUsd(){
+        return revenue_billion_usd;
+    }
+
+    public double getStockPriceChangePct(){
+        return stock_price_change_pct;
+    }
+
+    public double getGdpGrowthUsPct(){
+        return gdp_growth_us_pct;
+    }
+
+    public double getUnemploymentRateUsPct(){
+        return unemployment_rate_us_pct;
+    }
+
+    public boolean is_estimated(){
+        return is_estimated;
+    }
+
+    public String getConfidenceLevel(){
+        return confidence_level;
+    }
+
+    public int getDataQualityScore(){
+        return data_quality_score;
+    }
+}
+
+public class Project4 {
+
+}
